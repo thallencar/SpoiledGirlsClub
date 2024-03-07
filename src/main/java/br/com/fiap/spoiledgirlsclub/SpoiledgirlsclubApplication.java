3eca6baefbe44127ee7a40d4f9ba5953986ba5d4
marina-cucco <marinascucco@hotmail.com>
2024-03-06 22:38:48 -0300

    First commit

---
+++ src/main/java/br/com/fiap/spoiledgirlsclub/SpoiledgirlsclubApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.spoiledgirlsclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoiledgirlsclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoiledgirlsclubApplication.class, args);
+	}
+
+}
